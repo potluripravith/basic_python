8540dcbaba84004a974cc04e17a4ee15b30b1e9b
potluripravith <112188413+potluripravith@users.noreply.github.com>
2025-07-06 22:03:41 +0530

    Initial commit

---
+++ basic.py
@@ -0,0 +1,25 @@
+age = 38
+name = "Alice"
+height = 5.8
+is_student = False
+courses = ["Math", "Science"]
+
+# Control Flow
+if age >= 18:
+    print("Adult")
+elif age > 13:
+    print("Teen")
+    
+else:
+    print("Child")
+
+# Loops
+for course in courses:
+    print(f"Studying {course}")
+
+# Functions
+def calculate_area(length, width):
+    return length * width
+
+print(calculate_area(5, 4)) 
+
