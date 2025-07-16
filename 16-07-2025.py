6aad4c4bb2bd04bbe24b5c44bcaa6704a4fa18b7
potluripravith <112188413+potluripravith@users.noreply.github.com>
2025-07-16 22:51:56 +0530

    Practice

--- 16-07-2025.py
+++ 16-07-2025.py
@@ -5,6 +5,7 @@
 
 ## here it supports addition now we will add substraction and divison to it using decorator
 from functools import wraps
+
 def extend_operations(original_func):
     @wraps(original_func) #Preserves function metadata
     def wrapper(a,b,operation = 'add',**kargs):
@@ -18,6 +19,8 @@ def extend_operations(original_func):
             return original_func(a,b)
         
     return wrapper
+
+
 @extend_operations
 def calculate(a, b):
     return a + b
@@ -34,6 +37,8 @@ third_party_module.calculate = extend_operations(third_party_module.calculate)
 print(third_party_module.calculate(10, 5))  
 print(third_party_module.calculate(10, 5, operation='subtract'))  
 print(third_party_module.calculate(10, 4, operation='divide'))  
+
+
 ### Meta classes 
 
 class EnforceNaming(type):
@@ -44,4 +49,52 @@ class EnforceNaming(type):
         return super().__new__(cls,name,bases,dct)
 class Task(metaclass=EnforceNaming):
     def do_save(self): pass
-    def run(self):pass
+    # def run(self):pass
+    
+## cls MetaCLass itself
+## name name of the class being defined
+## bases Base classes of the new class
+##dct A dict of everything defined inside the class body
+
+###Protocols
+
+from typing import Protocol
+class Flyable(Protocol):
+    def fly(self)-> str:
+        ...
+        
+class bird():
+    def fly(self):
+        return "flying"
+    
+def launch(obj:Flyable):
+    print(obj.fly())
+    
+launch(bird())
+
+
+from typing import Protocol
+
+class Reader(Protocol):
+    def read(self)-> str:...
+class Writer(Protocol):
+    def write(self)->str:...
+    
+class readerwrite(Reader,Writer):
+    ...
+class File:
+    def read(self):
+        return "data"
+    def write(self):
+        print("written")
+        
+def test(obj:readerwrite):
+    print(obj.read())
+    obj.write()
+        
+test(File())
+        
+
+
+## LSP Subclasses must preserve base class behavior  
+## TO fix it refactor hierachy, split protocols
