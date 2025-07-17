7f2ed3dd8daed1b2ec79e3118c9d04da21f837a9
potluripravith <112188413+potluripravith@users.noreply.github.com>
2025-07-17 22:26:05 +0530

    Factory design pattern

--- 17-08-2025.py
+++ 17-08-2025.py
@@ -63,3 +63,72 @@ def notify_user(notification_type: str, message: str):
     notifier.send(message)
 
 notify_user("sms", "Welcome to the service!")
+
+
+## @staticmethod when we dont self which is instance method and cls which is classmethod
+ 
+### dependency inversion vs dependence injection youtube 
+
+### abc and protocols
+
+from abc import ABC, abstractmethod
+from typing import Protocol
+
+class Logger(Protocol):
+    def log (self,message:str):
+        ...
+        
+class PaymentGateway(ABC):
+    @abstractmethod
+    def authenticate(self):
+        pass
+
+    @abstractmethod
+    def pay(self, amount: float) :
+        pass
+
+
+class PayPalGateway(PaymentGateway):
+    def authenticate(self) :
+        print("Authenticated with PayPal.")
+        return True
+
+    def pay(self, amount: float) :
+        print(f"Paid Rs.{amount} using PayPal.")
+        
+        
+class StripeGateway(PaymentGateway):
+    def authenticate(self) :
+        print("Authenticated with Stripe.")
+        return True
+
+    def pay(self, amount: float):
+        print(f"Paid Rs.{amount} using Stripe.")
+        
+class ConsoleLogger:
+    def log(self, message: str):
+        print(f"[Console] {message}")
+        
+class FileLogger:
+    def log(self, message: str) :
+        with open("log.txt", "a") as f:
+            f.write(f"{message}\n")
+            
+class PaymentProcessor:
+    def __init__(self, gateway: PaymentGateway, logger: Logger):
+        self.gateway = gateway
+        self.logger = logger
+
+    def process_payment(self, amount: float):
+        if self.gateway.authenticate():
+            self.gateway.pay(amount)
+            self.logger.log(f"Payment of Rs. {amount} processed successfully.")
+        else:
+            self.logger.log("Authentication failed.")
+            
+if __name__ == "__main__":
+    paypal = PayPalGateway()
+    console_logger = ConsoleLogger()
+
+    processor = PaymentProcessor(paypal, console_logger)
+    processor.process_payment(5000.0)
